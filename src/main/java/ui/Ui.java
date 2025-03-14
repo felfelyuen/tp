ff94c98f0cbc3c1ac164b59d757984f866fc68fe
Betahaxer <yuwenpoh@gmail.com>
2025-03-14 22:43:50 +0800

    Add check for user input before reading inputs

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -1,11 +1,17 @@
 package ui;
 
+import static constants.CommandConstants.EXIT;
+
 import java.util.Scanner;
 
 public class Ui {
     private static final Scanner in = new Scanner(System.in);
     public static String getUserCommand() {
-        return in.nextLine();
+        if (in.hasNextLine()) {
+            return in.nextLine();
+        } else {
+            return EXIT; // Return a safe default to avoid exceptions
+        }
     }
 
     public static void showToUser(String message) {
