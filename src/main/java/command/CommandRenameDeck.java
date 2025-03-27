f7b2b4661e3b75002cf21e003bb0fdea34ae490a
Betahaxer <yuwenpoh@gmail.com>
2025-03-28 00:37:09 +0800

    Add Javadoc header comments for DeckManager and related command classes

--- src/main/java/command/CommandRenameDeck.java
+++ src/main/java/command/CommandRenameDeck.java
@@ -5,6 +5,11 @@ import static deck.DeckManager.renameDeck;
 import exceptions.FlashCLIArgumentException;
 import ui.Ui;
 
+/**
+ * Command to rename a selected deck.
+ *
+ * <p>Renames a deck to the provided new deck name. Displays either the success message or an error message based on the result.</p>
+ */
 public class CommandRenameDeck extends Command {
     private final String arguments;
 
@@ -12,6 +17,11 @@ public class CommandRenameDeck extends Command {
         this.arguments = arguments;
     }
 
+    /**
+     * Executes the command to rename the deck and shows the result to the user.
+     *
+     * <p>If an error occurs during renaming, an error message is displayed.</p>
+     */
     public void executeCommand() {
         try {
             Ui.showToUser(renameDeck(this.arguments));
--- src/main/java/command/CommandViewDecks.java
+++ src/main/java/command/CommandViewDecks.java
@@ -5,7 +5,18 @@ import static deck.DeckManager.viewDecks;
 import exceptions.FlashCLIArgumentException;
 import ui.Ui;
 
+/**
+ * Command to view all decks available.
+ *
+ * <p>Lists all decks that are available. Displays either the success message or an error message based on the result.</p>
+ */
 public class CommandViewDecks extends Command {
+
+    /**
+     * Executes the command to list decks and shows the result to the user.
+     *
+     * <p>If an error occurs during deck viewing, an error message is displayed.</p>
+     */
     public void executeCommand() {
         try {
             Ui.showToUser(viewDecks());
