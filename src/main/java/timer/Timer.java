9025cb014df679f6fad8145745f7219cf0cd65cb
Elon Koh <e0552367@u.nus.edu>
2025-03-29 20:15:10 +0800

    Add java doc descriptions

--- src/main/java/timer/Timer.java
+++ src/main/java/timer/Timer.java
@@ -1,15 +1,27 @@
 package timer;
 
+import java.util.ArrayList;
+
+/**
+ * Represents a timer that contains the timestamp of creation.
+ * <p>This class provides the functionality of keeping track of time elapsed.</p>
+ */
 public class Timer {
     private long startTime;
+    private final ArrayList<Long> timestamps = new ArrayList<>();
 
     public Timer(long startTime) {
         this.startTime = startTime;
     }
 
+    /**
+     * Calculates the difference in time between object creation and method called.
+     *
+     * @return the time duration in seconds
+     */
     public long getDuration() {
         long currentTime = System.nanoTime();
+        timestamps.add(currentTime);
         return ((currentTime - startTime)/1000000000);
-
     }
 }
