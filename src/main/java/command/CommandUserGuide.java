2393d79064a116b8762b56472085d575185d4ff8
FelixYuen <e1121462@u.nus.edu>
2025-03-31 10:37:17 +0800

    Added javadocs

--- src/main/java/command/CommandUserGuide.java
+++ src/main/java/command/CommandUserGuide.java
@@ -1,9 +1,12 @@
 package command;
 
-import ui.Ui;
-
 import static constants.UserGuideMessages.USER_GUIDE_INFORMATION;
 
+import ui.Ui;
+
+/**
+ * Class to print user guide information
+ */
 public class CommandUserGuide extends Command{
 
     @Override
