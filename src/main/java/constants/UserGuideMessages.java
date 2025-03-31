23dac345c43de29b54bdab3c1d666d8b48118a76
Betahaxer <yuwenpoh@gmail.com>
2025-03-31 13:08:10 +0800

    Update UserGuideMessages

--- src/main/java/constants/UserGuideMessages.java
+++ src/main/java/constants/UserGuideMessages.java
@@ -1,11 +1,12 @@
+//@@author felfelyuen
 package constants;
 
-import static constants.CommandConstants.CREATE;
-import static constants.CommandConstants.DELETE;
-import static constants.CommandConstants.EDIT;
+import static constants.CommandConstants.ADD_CARD;
+import static constants.CommandConstants.DELETE_CARD;
+import static constants.CommandConstants.EDIT_CARD;
 import static constants.CommandConstants.EXIT;
 import static constants.CommandConstants.INSERT_CODE;
-import static constants.CommandConstants.LIST;
+import static constants.CommandConstants.LIST_CARDS;
 import static constants.CommandConstants.MARK_LEARNED;
 import static constants.CommandConstants.MARK_UNLEARNED;
 import static constants.CommandConstants.NEW_DECK;
@@ -25,14 +26,14 @@ public class UserGuideMessages {
             "Quick Start:\n" +
                     "Create a deck of flashcards with \"" + NEW_DECK +
                     "\", select it with \"" + SWITCH_DECK +
-                    "\", and begin adding flashcards with \"" + CREATE + "\"!\n\n" +
+                    "\", and begin adding flashcards with \"" + ADD_CARD + "\"!\n\n" +
                     "List of commands:\n" +
-                    "\"" + CREATE + "\": creates a flashcard\n" +
-                    "\"" + LIST + "\": shows list of flashcards\n" +
+                    "\"" + ADD_CARD + "\": creates a flashcard\n" +
+                    "\"" + LIST_CARDS + "\": shows list of flashcards\n" +
                     "\"" + VIEW_QN + "\": views the flashcard's question\n" +
                     "\"" + VIEW_ANS + "\": views the flashcard's answer\n" +
-                    "\"" + DELETE + "\": deletes the flashcard\n" +
-                    "\"" + EDIT + "\": edits the flashcard question/answer\n" +
+                    "\"" + DELETE_CARD + "\": deletes the flashcard\n" +
+                    "\"" + EDIT_CARD + "\": edits the flashcard question/answer\n" +
                     "\"" + INSERT_CODE + "\": inserts code snippet for a flashcard\n" +
                     "\"" + MARK_LEARNED + "\": marks flashcard as learned\n" +
                     "\"" + MARK_UNLEARNED + "\": marks flashcard as unlearned\n" +
